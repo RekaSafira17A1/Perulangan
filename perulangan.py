1f7cf05b2995ccab71959bb9320b177bce23214d
RekaSafira17A1 <37066840+RekaSafira17A1@users.noreply.github.com>
2018-03-19 00:51:21 -0700

    Add files via upload

---
+++ perulangan.py
@@ -0,0 +1,25 @@
+print("              Perulangan \n \n")
+
+print ("1. Foor Loop \n")
+
+#program1
+i = 10
+for i in range (10):
+	print(i + 3)
+	i = i + 1
+print"\n"
+#program2
+for huruf in 'PYTHON':
+	print(huruf)
+print"\n"
+#program3
+pelajaran = ('kimia', 'fisika', 'biologi')
+for belajar in pelajaran:
+	print("IPA : ", belajar)
+	
+print("\n 2. While Loop \n")
+i=1
+while (i<11):
+	print (i)
+	i = i + 1
+	
